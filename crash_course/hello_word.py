1fd3476fea28f6aa092fabba76ff0734afb3dd24
duy.ong <duy.ong@aurionpro.com>
2025-10-01 00:01:57 +0700

    add example for list

---
+++ crash_course/hello_word.py
@@ -0,0 +1,5 @@
+last_name = "Ong"
+first_name = "Duy"
+num_orange, num_purple = 3, 4
+print (f"Hello world! \"{first_name}\" {last_name}")
+print (f" These are {num_orange} oranges and {num_purple} purples.")
---
+++ crash_course/list_example.py
@@ -0,0 +1,32 @@
+users = ["user1", "user2", "user3"]
+print (users)
+users.append("user4")
+print (users)
+
+users.insert(4,"user5")
+print (users)
+
+del users[0]
+print (users)
+users.reverse()
+print (users)
+
+print (len(users))
+
+for user in users:
+    print (user)
+
+for i in range (1, 5):
+    print (i)
+
+
+numbers = [1,2,3,4,5]
+print (f"Max number is {max(numbers)}")
+print (f"Min number is {min(numbers)}")
+
+x2Numbers = [value * 2 for value in numbers]
+print (x2Numbers)
+
+#slide a list
+slicedNumbers = numbers[1:3]
+print (slicedNumbers)
